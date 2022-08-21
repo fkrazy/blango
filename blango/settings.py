c7583b5bb7b6247bb603a5433ca84d19b5868582
una cientifica <f2kloa@gmail.com>
2022-08-21 06:19:07 +0000

    finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -181,8 +181,14 @@ class Dev(Configuration):
     "root": {
         "handlers": ["console"],
         "level": "DEBUG",
+        }
     }
-}
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 class Prod(Dev):
     DEBUG = False
